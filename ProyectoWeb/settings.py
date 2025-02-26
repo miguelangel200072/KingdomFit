5685ba0346c71875d8c3596710352d2dab2b8b73
miguelangel200072 <119166801+miguelangel200072@users.noreply.github.com>
2025-02-26 20:54:45 +0100

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="miguelanrg@gmail.com"
-EMAIL_HOST_PASSWORD="lcll mlbf mmnw vhtl"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
